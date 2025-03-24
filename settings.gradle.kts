ad48c6a6555ed8bc0b5083ccb26addd92e972770
corrupter76 <140513866+corrupter76@users.noreply.github.com>
2025-03-24 14:34:06 +0800

    Add files via upload
    this Android project is about a music app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+rootProject.name = "AA_music_0309"
+include(":app")
+ 
